da2efbe3294eedbea1a27223b3737ffe26c08557
JKNH <juancmunoz909@gmail.com>
2024-04-22 11:23:45 -0500

    .

--- js/btnscript.js
+++ js/btnscript.js
@@ -1,4 +1,14 @@
-document.getElementById(2).addEventListener(click, getStudent, onclick)
+document.getElementById(2).addEventListener("click", getStudent(), onclick)
+const url =  "https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/students"
 
-
-
+async function getStudent(){
+    if(document.getElementById(1).textContent==="1234"){
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {      'Content-Type': 'application/json' 
+            },
+        body:JSON.stringify(payload)
+        })
+        console.log(response)
+    }
+}
