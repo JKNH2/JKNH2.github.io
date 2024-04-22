6c4af93f5cf5e86f974f24be6ffdda64e64fb5ea
JKNH <juancmunoz909@gmail.com>
2024-04-22 11:38:56 -0500

    .

--- js/btnscript.js
+++ js/btnscript.js
@@ -1,14 +1,19 @@
-document.getElementById(2).addEventListener("click", getStudent(), onclick)
+const btn = document.getElementById(2).addEventListener("click", getStudent(), onclick)
 const url =  "https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/students"
 
-async function getStudent(){
+function getStudent(){
     if(document.getElementById(1).textContent==="1234"){
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {      'Content-Type': 'application/json' 
-            },
-        body:JSON.stringify(payload)
-        })
-        console.log(response)
+        let res = makeStudentPayload(url, document.getElementById(0))
+        document.getElementById(3).textContent = res
     }
+}
+
+function makeStudentPayload(url, payload){
+    const response = fetch(url, {
+        method: 'POST',
+        headers: {      'Content-Type': 'application/json' 
+        },
+    body:JSON.stringify(payload)
+    })
+    return response.clave
 }
