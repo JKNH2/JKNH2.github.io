e848625819352ca72e5b4f074ecc86e54f5a13e8
JKNH <juancmunoz909@gmail.com>
2024-04-22 11:57:37 -0500

    .

--- js/btnscript.js
+++ js/btnscript.js
@@ -1,19 +1,30 @@
 const btn = document.getElementById(2).addEventListener("click", getStudent(), onclick)
-const url =  "https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/students"
+const url = "https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/students"
 
-function getStudent(){
-    if(document.getElementById(1).textContent==="1234"){
-        let res = makeStudentPayload(url, document.getElementById(0))
-        document.getElementById(3).textContent = res
+function getStudent() {
+    let codigo = document.getElementById(0).textContent
+    let contra = document.getElementById(0).textContent
+    if (contra == 1234) {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify({ codigo }) 
+        };
+
+        fetch(url, options)
+            .then(response => response.json()) 
+            .then(data => {
+                if (data.hasOwnProperty('clave')) { 
+                    console.log('Valor de clave:', data.clave); 
+                } else {
+                    console.error('Atributo clave no encontrado en la respuesta');
+                }
+            })
+            .catch(error => document.getElementById(4).textContent="codigo o contraseña invalidos");
     }
+    document.getElementById(4).textContent="codigo o contraseña invalidos"
 }
 
-function makeStudentPayload(url, payload){
-    const response = fetch(url, {
-        method: 'POST',
-        headers: {      'Content-Type': 'application/json' 
-        },
-    body:JSON.stringify(payload)
-    })
-    return response.clave
-}
+
